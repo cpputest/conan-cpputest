e918e545be0b335d3af8b5c8602c5fa9a9b4b3e1
Bryce Schober <bryce.schober@dynonavionics.com>
2018-07-31 08:44:26 -0700

    Add default username and channel for builds

--- build.py
+++ build.py
@@ -2,6 +2,6 @@ from conan.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
-    builder.add_common_builds(shared_option_name="CppUTest:shared")
+    builder = ConanMultiPackager(username="bryceschober", channel="testing")
+    builder.add_common_builds()
     builder.run()

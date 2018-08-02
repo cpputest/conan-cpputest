9b8bc3d1b1145c92ad2967127070f4bf029a527a
Bryce Schober <bryce.schober@dynonavionics.com>
2018-08-01 20:38:39 -0700

    Set multi-build parameters for upload

--- build.py
+++ build.py
@@ -2,6 +2,9 @@ from conan.packager import ConanMultiPackager
 
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager()
+    builder = ConanMultiPackager(username="bryceschober", 
+        upload="https://api.bintray.com/conan/bryceschober/bks-conan",
+        stable_branch_pattern="release*",
+        channel="testing")
     builder.add_common_builds()
     builder.run()
